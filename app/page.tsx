4ff1112336ec4f95deae1bd709ffc00fab5bad02
jperaino <jim@spatiometrics.com>
2024-03-30 11:32:36 -0700

    add external links

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ModeToggle } from "@/components/core/ModeToggle";
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -11,8 +12,24 @@ export default function Home() {
             <p className="text-s">Software engineer</p>
           </div>
           <div className="space-x-1">
-            <ModeToggle />
-            <ModeToggle />
+            <a
+              href="https://github.com/jperaino"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button variant="ghost" size="icon">
+                <FaGithub className="h-[1.2rem] w-[1.2rem] " />
+              </Button>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/jim-peraino/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button variant="ghost" size="icon">
+                <FaLinkedin className="h-[1.2rem] w-[1.2rem] " />
+              </Button>
+            </a>
             <ModeToggle />
           </div>
         </div>
